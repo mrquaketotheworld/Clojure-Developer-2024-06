a6870fac77470746d6fc2a4d2d41ed2932a8e9c7
mrquaketotheworld <newrushiflyer@gmail.com>
2024-07-25 12:57:00 +0300

    Add OTUS-04 HW

--- otus-04/src/otus_04/homework/magic_square.clj
+++ otus-04/src/otus_04/homework/magic_square.clj
@@ -14,4 +14,24 @@
   Магический квадрат должен быть заполнен так, что суммы всех вертикалей,
   горизонталей и диагоналей длиной в n должны быть одинаковы."
   [n]
-  [[0]])
+  (let [square (vec (repeat n (vec (repeat n 0))))
+        place-number (fn [square num row col]
+                       (assoc-in square [row col] num))
+        next-pos (fn [row col]
+                   [(mod (- row 1) n) (mod (+ col 1) n)])]
+    (loop [i 1
+           row 0
+           col (quot n 2)
+           square square]
+      (if (> i (* n n))
+        square
+        (let [[new-row new-col] (next-pos row col)
+              [new-row new-col] (if (not= 0 (get-in square [new-row new-col]))
+                                  [(mod (+ row 1) n) col]
+                                  [new-row new-col])]
+          (recur (inc i)
+                 new-row
+                 new-col
+                 (place-number square i row col)))))))
+
+; Эту задачу я решил с помощью ChatGPT, ибо самому на данном этапе мне её не решить никогда.
--- otus-04/src/otus_04/homework/scramblies.clj
+++ otus-04/src/otus_04/homework/scramblies.clj
@@ -1,4 +1,5 @@
-(ns otus-04.homework.scramblies)
+(ns otus-04.homework.scramblies
+  (:require [clojure.string :as string]))
 
 ;; Оригинальная задача:
 ;; https://www.codewars.com/kata/55c04b4cc56a697bb0000048
@@ -7,4 +8,13 @@
   "Функция возвращает true, если из букв в строке letters
   можно составить слово word."
   [letters word]
-  nil)
+  (loop [word-map (group-by identity word)
+         letters-map (group-by identity letters)]
+    (let [pair-word-map (first word-map)
+          letter (first pair-word-map)
+          letter-count (count (second pair-word-map))]
+      (if (empty? word-map)
+        true
+        (if (and (letters-map letter) (>= (count (letters-map letter)) letter-count))
+          (recur (dissoc word-map letter) (dissoc letters-map letter))
+          false)))))
